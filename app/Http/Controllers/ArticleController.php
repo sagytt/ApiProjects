d13cbdcbce9df940b90f345f3140dec5337a23d8
$agy <sagix7004@gmail.com>
2020-04-26 15:51:22 +0300

    resource part

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Article;
+use App\Http\Resources\Article as ArticleResource;
+use Illuminate\Routing\Controller;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Get Articles
+        $articles = Article::paginate(15);
+        //print_r($articles);die();
+        //Return collection of articles as resource
+        return ArticleResource::collection($articles);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //Get single article
+
+        $article = Article::findOrFail($id);
+
+        //Return single article as a resource
+
+        return new ArticleResource($article);
+
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,23 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/v1/flights', v1\FlightController::class, [
-    'except'  => ['create', 'edit']
-]);
+//Route::resource('/v1/flights', v1\FlightController::class, [
+//    'except'  => ['create', 'edit']
+//]);
+
+
+//List Articles
+Route::get('article', 'ArticleController@index');
+
+//List single Articles
+Route::get('article/{id}', 'ArticleController@show');
+
+//Crete new Articles
+Route::post('article', 'ArticleController@store');
+
+//Update Articles
+Route::put('articles', 'ArticleController@store');
+
+//Update Articles
+Route::delete('articles', 'ArticleController@destroy');
+
